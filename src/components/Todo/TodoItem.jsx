a9f49965b376d2b6ccfc2c627301cff3780a0570
dokimion24 <leecy3610@gmail.com>
2023-03-02 01:23:24 +0900

    Feat: Button 컴포넌트 적용, 체크 버튼 클릭 시 아이콘 토글

--- src/components/Todo/TodoItem.jsx
+++ src/components/Todo/TodoItem.jsx
@@ -1,27 +1,66 @@
-import React from "react";
-import { BsCheckCircle, BsCircle, BsTrash, BsPencil } from "react-icons/bs";
+import React, { useState } from 'react';
+import { editTodo } from '../../api/api';
+import {
+  BsCheckCircle,
+  BsCircle,
+  BsTrash,
+  BsPencil,
+  BsCheck,
+} from 'react-icons/bs';
+import styled from 'styled-components';
+
+const TodoItemBlock = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 14px;
+  background-color: #f5f5f5;
+  border-radius: 10px;
+  margin-bottom: 10px;
+`;
+
+const Button = styled.button`
+  display: ${(props) => (props.isDone ? 'inline' : 'none')};
+`;
 
 const TodoItem = ({ todoItem }) => {
-  console.log(todoItem);
-  const { title } = todoItem;
+  // const { title, done } = todoItem;
+
+  const [todo, setTodo] = useState(todoItem);
+
+  const handleClickEditBtn = (e) => {
+    console.log(todo);
+  };
+
+  const toggleTodoDone = async () => {
+    setTodo({ ...todo, done: !todo.done });
+    await editTodo(todo);
+  };
+
   return (
-    <div>
+    <TodoItemBlock>
       <div>
-        <button>
-          <BsCheckCircle /> <BsCircle />
-        </button>
-        <span>{title}</span>
-        <input type="text" />
+        <Button isDone={todo.done} onClick={() => toggleTodoDone(todo.done)}>
+          <BsCheckCircle />
+        </Button>
+        <Button isDone={!todo.done} onClick={() => toggleTodoDone(todo.done)}>
+          <BsCircle />
+        </Button>
+        <span>{todo.title}</span>
+        <input type="text" style={{ display: 'none' }} />
       </div>
       <div>
         <button>
-          <BsPencil />
+          <BsPencil onClick={handleClickEditBtn} />
+        </button>
+        <button>
+          <BsCheck />
         </button>
         <button>
           <BsTrash />
         </button>
       </div>
-    </div>
+    </TodoItemBlock>
   );
 };
 
