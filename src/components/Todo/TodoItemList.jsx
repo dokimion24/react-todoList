2af53eac8a817dae8265470113e76464cee0eafd
dokimion24 <leecy3610@gmail.com>
2023-03-01 16:07:46 +0900

    Feat: Todo 전체 목록 가져와 화면에 출력
    fetchTodo 함수 : Todo data 가져옴
    로딩, 에러 예외 처리

--- src/components/Todo/TodoItemList.jsx
+++ src/components/Todo/TodoItemList.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import styled from "styled-components";
+
+import TodoItem from "./TodoItem";
 
 const TodoItemListBlock = styled.div`
   flex: 1;
@@ -7,11 +10,65 @@ const TodoItemListBlock = styled.div`
   background: gray; /* 사이즈 조정이 잘 되고 있는지 확인하기 위한 임시 스타일 */
 `;
 
-
-
-
 const TodoItemList = () => {
-  return <TodoItemListBlock>TodoList</TodoItemListBlock>;
+  const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsloading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchTodos = async () => {
+    setIsloading(true);
+    setError(null);
+    try {
+      const response = await axios.get(
+        "https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos",
+        {
+          headers: {
+            "content-type": "application/json",
+            apikey: "FcKdtJs202301",
+            username: "KDT4_LeeChanYoung",
+          },
+        }
+      );
+      setIsloading(false);
+      return response.data;
+    } catch (error) {
+      setIsloading(false);
+      setError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      const todo = await fetchTodos();
+      setTodoList(todo);
+    })();
+  }, []);
+
+  let content = <p>할 일을 추가해주세요</p>;
+
+  if (todoList?.length > 0) {
+    content = (
+      <ul>
+        {todoList.map((todoItem) => (
+          <TodoItem key={todoItem.id} todoItem={todoItem} />
+        ))}
+      </ul>
+    );
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  return (
+    <TodoItemListBlock>
+      <div>{content}</div>
+    </TodoItemListBlock>
+  );
 };
 
 export default TodoItemList;
