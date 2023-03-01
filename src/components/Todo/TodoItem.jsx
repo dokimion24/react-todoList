49aa1f04f418e4525c455d1e78e1e773b27f9c0d
dokimion24 <leecy3610@gmail.com>
2023-03-01 16:06:37 +0900

    Feat: TodoItem 컴포넌트 생성

--- src/components/Todo/TodoItem.jsx
+++ src/components/Todo/TodoItem.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
-const TodoItem = () => {
-  return <div>TodoItem</div>;
+const TodoItem = ({ todoItem }) => {
+  console.log(todoItem);
+  const { title } = todoItem;
+  return (
+    <div>
+      <div>
+        <span>체크</span>
+        <span>{title}</span>
+        <input type="text" />
+      </div>
+      <div>
+        <span>수정</span>
+        <span>완료</span>
+        <span>삭제</span>
+      </div>
+    </div>
+  );
 };
 
 export default TodoItem;
