c61ee9fc9502efc3716b19a51418ed23d6edb57b
dokimion24 <leecy3610@gmail.com>
2023-03-02 11:27:45 +0900

    Feat: TodoForm 컴포넌트 태그 작성

--- src/components/Todo/TodoForm.jsx
+++ src/components/Todo/TodoForm.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React from 'react';
 
-const TodoForm = () => {
-  return <div>TodoForm</div>;
+const TodoForm = (handleAddTodo) => {
+  return (
+    <form>
+      <input type="text" />
+      <button type="sumbmit" onSubmit={handleAddTodo}>
+        Add
+      </button>
+    </form>
+  );
 };
 
 export default TodoForm;
