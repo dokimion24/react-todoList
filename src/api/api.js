593a736824f4e9857272594970121962917de331
dokimion24 <leecy3610@gmail.com>
2023-03-02 19:57:19 +0900

    Feat: Todo 추가 함수 작성 - addTodo

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,29 @@
-import axios from 'axios';
+import axios from "axios";
 
 const headers = {
   apikey: `${import.meta.env.VITE_API_KEY}`,
   username: `${import.meta.env.VITE_USERNAME}`,
 };
 
+console.log(`${import.meta.env.VITE_BASE_URL}`);
 export const fetchTodos = async () => {
   const response = await axios.get(`${import.meta.env.VITE_BASE_URL}`, {
     headers,
   });
+
+  return response.data;
+};
+
+export const addTodo = async (title) => {
+  const response = await axios.post(
+    `${import.meta.env.VITE_BASE_URL}`,
+    {
+      title,
+    },
+    {
+      headers,
+    }
+  );
   return response.data;
 };
 
