e502f1697198cba6d9d7694ced2c19262ea216de
dokimion24 <leecy3610@gmail.com>
2023-03-04 07:22:54 +0900

    Feat: 날짜 형식 지정 함수 작성

---
+++ src/constants/formatDate.js
@@ -0,0 +1,8 @@
+export const formattedDate = (todoDate) => {
+  // const date = new Date(todoDate);
+  // const year =
+  // const month = String(date.getMonth() + 1).padStart(2, '0');
+  // const today = String(date.getDate()).padStart(2, '0');
+
+  // return `${month}/${today} ${hour}:${min}`;
+};
