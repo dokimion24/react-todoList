3ed5d4410adfa24c53540450dba37a7984b085ec
dokimion24 <leecy3610@gmail.com>
2023-03-04 18:25:32 +0900

    Feat: formatDate.js -> getDate.js

---
+++ src/constants/getDate.js
@@ -0,0 +1,10 @@
+export const getDateObj = (ClickedDate) => {
+  const dateObj = {
+    year: String(ClickedDate.getFullYear()),
+    month: String(ClickedDate.getMonth() + 1).padStart(2, '0'),
+    date: String(ClickedDate.getDate()).padStart(2, '0'),
+    day: new Intl.DateTimeFormat('ko-KR', { weekday: 'long' }).format(ClickedDate),
+  }
+
+  return dateObj
+}
